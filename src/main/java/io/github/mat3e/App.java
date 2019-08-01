684cccae9e232e9ccb9bd1ffdc7f21d74dc7b225
Piotr.Chmiel <piotr.chmiel@wolterskluwer.com>
2019-08-01 15:29:27 +0200

    git test

--- src/main/java/io/github/mat3e/App.java
+++ src/main/java/io/github/mat3e/App.java
@@ -8,6 +8,6 @@ import org.slf4j.LoggerFactory;
 public class App {
     public static void main(String[] args) {
         var logger = LoggerFactory.getLogger(App.class);
-        System.out.println("Hello World!");
+        System.out.println("Hello World!!");
     }
 }
